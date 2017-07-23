8d49e8bf10bb7914e62b1d9ce0fefdc775b05da7
Programmering <barthipost@gmail.com>
2017-07-23 21:58:43 +0200

    Tilføjede simpel respawn og bedre fysik.

--- Unity/Gruppe 4/Assets/Scripts/PlayerBehaviour/PlayerBehaviour.cs
+++ Unity/Gruppe 4/Assets/Scripts/PlayerBehaviour/PlayerBehaviour.cs
@@ -6,11 +6,24 @@ public class PlayerBehaviour : MonoBehaviour {
     public string horizontalAxis;
     public string verticalAxis;
     public string chargeDashAxis;
+    [Space(20)]
+    public float chargeMax = 5;
+    public float pushForce = 10;
     public float walkSpeed = 1;
-    public Rigidbody body;
-    public float chargeStart = 0;
-    public bool charging = false;
     public float dashForce = 25;
+    [Space(20)]
+    public Vector3 spawn; 
+    [Space(20)]
+    public bool charging = false;
+    public bool onGround = false;
+    public float chargeStart = 0;
+    public float chargeDir = 0;
+    [Space(20)]
+    public Rigidbody body;
+    public Collider collision;
+
+
+
 
     void Awake ()
     {
@@ -26,19 +39,29 @@ public class PlayerBehaviour : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
+        Charge();
         //print(Input.GetAxis(chargeDashAxis)+ " " + Input.GetAxis(verticalAxis) + " " + Input.GetAxis(horizontalAxis));
-	}
+    }
 
     void FixedUpdate()
     {
         Move(walkSpeed);
-        Charge();
+        if (transform.position.y < -10)
+        {
+            KO();
+        }
+        
     }
 
+    
 
     public void Move(float speed)
     {
-        body.velocity = new Vector3(body.velocity.x + speed * Input.GetAxis(horizontalAxis), body.velocity.y, body.velocity.z);
+        if (onGround)
+        {
+
+            body.velocity = new Vector3(body.velocity.x + speed * Input.GetAxis(horizontalAxis), body.velocity.y, body.velocity.z);
+        }
     }
 
     public void Charge()
@@ -63,7 +86,41 @@ public class PlayerBehaviour : MonoBehaviour {
 
     public void Dash()
     {
-        float chargeTime = Time.time - chargeStart;
+        float chargeTime = Mathf.Min(chargeMax, Time.time - chargeStart);
         body.velocity = body.velocity + new Vector3(0, dashForce * chargeTime, 0);
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.tag == gameObject.tag)
+        {
+            Rigidbody other = col.gameObject.GetComponent<Rigidbody>();
+            if (other.velocity.magnitude > body.velocity.magnitude)
+            {
+                body.velocity = body.velocity + (body.position - other.position).normalized * pushForce * other.velocity.magnitude;
+            }
+        }
+    }
+
+    private void OnCollisionStay(Collision col)
+    {
+        if (col.gameObject.tag != gameObject.tag)
+        {
+            onGround = true;
+        }
+    }
+    private void OnCollisionExit(Collision col)
+    {
+        if (col.gameObject.tag != gameObject.tag)
+        {
+            onGround = false;
+        }
+    }
+
+    public void KO()
+    {
+        transform.position = spawn;
+        body.velocity = new Vector3(0, 0, 0);
+        body.angularVelocity = new Vector3(0, 0, 0);
     }
 }
