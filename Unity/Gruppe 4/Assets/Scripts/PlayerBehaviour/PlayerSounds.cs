8fcc08914f21b683bc793ade2468731f39475004
Programmering <barthipost@gmail.com>
2017-07-25 20:46:41 +0200

    Ændrede kontrol og fysik.

---
+++ Unity/Gruppe 4/Assets/Scripts/PlayerBehaviour/PlayerSounds.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSounds : MonoBehaviour {
+
+    public AudioClip dash;
+    public AudioClip hitGround;
+    public AudioClip hitOther;
+    
+	// Use this for initialization
+	void Start ()
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
