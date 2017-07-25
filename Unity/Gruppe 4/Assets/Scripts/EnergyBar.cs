efc7882b95261301b8a4a9fdec8c845a13271650
Programmering <barthipost@gmail.com>
2017-07-25 15:53:35 +0200

    Færdiggjorde energisystemet

---
+++ Unity/Gruppe 4/Assets/Scripts/EnergyBar.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class EnergyBar : MonoBehaviour {
+
+    public GameObject player;
+    private PlayerBehaviour playerBehaviour;
+
+    void Awake()
+    {
+        playerBehaviour = player.GetComponent<PlayerBehaviour>();
+    }
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.localScale = new Vector3(transform.localScale.x, playerBehaviour.energy / playerBehaviour.maxEnergy, 1);
+	}
+}
--- Unity/Gruppe 4/Assets/Scripts/NewBehaviourScript.cs
+++ Unity/Gruppe 4/Assets/Scripts/NewBehaviourScript.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class INGAMEGUI : MonoBehaviour {
+public class NewBehaviourScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
