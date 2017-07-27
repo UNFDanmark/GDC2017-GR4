7e3c1909f985b595f835976dabcbe9eb56ad7d7b
3D <theodor.risager@gmail.com>
2017-07-27 22:44:58 +0200

    Finnishe all of winner scene.

--- 3D/UnityTest/Assets/VictoryScene/Scripts/WinnerScreenControlller.cs
+++ 3D/UnityTest/Assets/VictoryScene/Scripts/WinnerScreenControlller.cs
@@ -2,45 +2,121 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class WinnerScreenControlller : MonoBehaviour {
-
-    bool winnerIsBlue = false;
+public class WinnerScreenControlller : MonoBehaviour
+{
+    public bool winnerIsBlue = false;
+    public int blueKills = 19;
+    public int redKills = 78;
 
     [Header("References")]
     public Image blueHead;
     public Image redHead;
     public SpriteRenderer backGround;
 
-    public GameObject character;
+    public GameObject blueCharacter;
+    public GameObject redCharacter;
+    public GameObject confetti;
 
     [Header("Blue")]
     public Sprite blueHeadWinner;
     public Sprite blueHeadLoser;
     public Sprite blueBackground;
-    public GameObject blueCharacter;
     public GameObject blueKillList;
-    public Texture blueConfetti;
+    public Material blueConfetti;
 
     [Header("Red")]
     public Sprite redHeadWinner;
     public Sprite redHeadLoser;
     public Sprite redBackground;
-    public GameObject redCharacter;
     public GameObject redKillList;
-    public Texture redConfetti;
+    public Material redConfetti;
 
+    void Start()
+    {
+        GetValues();
+    
+        SetColors();
+    }
 
-    public void BMenu() {
+    public void BMenu()
+    {
         // MENU
     }
-    public void BRematch() {
+    public void BRematch()
+    {
         // DOJO
     }
 
-    // Blue Winner.
-    void BlueWinner() {
-        
+    // Finds all data needed.
+    void GetValues() {
+        winnerIsBlue = false;
+        blueKills = 10;
+        redKills = 10;
     }
 
+    // Sets colors.
+    void SetColors()
+    {
 
+        if (winnerIsBlue)
+        {
+            // Blue Winner.
+            // Blue.
+            blueHead.sprite = blueHeadWinner;
+            backGround.sprite = blueBackground;
+            blueCharacter.SetActive(true);
+            confetti.GetComponent<ParticleSystemRenderer>().material = blueConfetti;
+
+            // Red.
+            redHead.sprite = redHeadLoser;
+            redCharacter.SetActive(false);
+        }
+        else
+        {
+            // Red Winner.
+            // Red.
+            redHead.sprite = redHeadWinner;
+            backGround.sprite = redBackground;
+            redCharacter.SetActive(true);
+            confetti.GetComponent<ParticleSystemRenderer>().material = redConfetti;
+
+            // Blue.
+            blueHead.sprite = blueHeadLoser;
+            blueCharacter.SetActive(false);
+        }
+
+        SetKills(blueKillList, blueKills);
+        SetKills(redKillList, redKills);
+
+    }
+
+    // Setup kill list.
+    void SetKills(GameObject killList, int amount)
+    {
+        // Reset icons.
+        for (int i = 0; i < killList.transform.childCount; i++)
+        {
+            Transform child = killList.transform.GetChild(i);
+            for (int j = 0; j < child.childCount; j++)
+            {
+                child.GetChild(j).gameObject.SetActive(false);
+            }
+        }
+
+        // Set icons.
+        int set = 0;
+        for (int i = 0; i < killList.transform.childCount; i++)
+        {
+            Transform child = killList.transform.GetChild(i);
+            for (int j = 0; j < child.childCount; j++)
+            {
+                child.GetChild(j).gameObject.SetActive(true);
+                set++;
+                if (set == amount)
+                    return;
+            }
+        }
+        
+
+    }
 }
