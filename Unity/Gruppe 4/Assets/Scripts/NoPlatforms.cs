6a6c453a130a8f408ea4c48ebd87501fa6d4832a
Programmering <barthipost@gmail.com>
2017-07-27 22:06:23 +0200

    Tilføjede sudden death mechanics

---
+++ Unity/Gruppe 4/Assets/Scripts/NoPlatforms.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NoPlatforms : MonoBehaviour {
+    public GameObject main;
+    private Main mainScript;
+    // Use this for initialization
+    void Awake()
+    {
+        mainScript = main.GetComponent<Main>();
+    }
+
+    void Start () {
+	    if(!mainScript.globalVariables.platforms)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
