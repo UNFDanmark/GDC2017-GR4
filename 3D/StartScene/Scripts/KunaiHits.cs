fbe2adba9f4c8cb7c11e0cb397417b7f42442fb8
3D <theodor.risager@gmail.com>
2017-07-27 17:46:59 +0200

    Finished animating and setting up sounds for Start scene

---
+++ 3D/StartScene/Scripts/KunaiHits.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KunaiHits : MonoBehaviour
+{
+    public AudioController speaker;
+    bool played = false;
+
+    private void Update()
+    {
+    // Will play a sound when it collides with the pole.
+        if (!played)
+            if (transform.position.x <= -9)
+            {
+                speaker.Play("kunai hit sound");
+                played = true;
+            }
+    }
+}
